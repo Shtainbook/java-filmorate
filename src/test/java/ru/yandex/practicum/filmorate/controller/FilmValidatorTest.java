738acaeb156f7f22f720736b8ca6d081277048d1
Shtainbook <shtainbook@yandex.ru>
2023-01-22 20:01:30 +0300

    поправил тесты

--- src/test/java/ru/yandex/practicum/filmorate/controller/FilmValidatorTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/controller/FilmValidatorTest.java
@@ -2,7 +2,7 @@ package ru.yandex.practicum.filmorate.controller;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import ru.yandex.practicum.filmorate.exception.FilmValidationException;
+import ru.yandex.practicum.filmorate.exception.EntityValidationException;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.validator.FilmValidator;
 
@@ -11,38 +11,38 @@ import java.time.LocalDate;
 class FilmValidatorTest {
     @Test
     public void validationFilmThrowExceptionWhenFilmNameIsBlank() {
-        FilmValidationException exception = Assertions.assertThrows(FilmValidationException.class, () ->
-               FilmValidator.validationOfFilm(
-                       new Film(1, " ", "эротодрамма", LocalDate.of(2005, 12, 22), 45)));
+        EntityValidationException exception = Assertions.assertThrows(EntityValidationException.class, () ->
+                FilmValidator.validationOfFilm(
+                        new Film(1, " ", "эротодрамма", LocalDate.of(2005, 12, 22), 45)));
 
-        Assertions.assertEquals("Название не должно быть пустым", exception.getMessage());
+        Assertions.assertEquals("class ru.yandex.practicum.filmorate.exception.EntityValidationException Название не должно быть пустым", exception.getMessage());
     }
 
     @Test
     public void validationFilmThrowExceptionWhenWhenDescriptionToMuchLong() {
-        FilmValidationException exception = Assertions.assertThrows(FilmValidationException.class, () ->
+        EntityValidationException exception = Assertions.assertThrows(EntityValidationException.class, () ->
                 FilmValidator.validationOfFilm(
                         new Film(1, "медвежуть", "путинмедведевшойгунавсегдапутинмедведевшойгунавсегдапутинмедведевшойгунавсегдапутинмедведевшойгунавсегдапутинмедведевшойгунавсегдапутинмедведевшойгунавсегдапутинмедведевшойгунавсегдапутинмедведевшойгунавсегдапутинмедведевшойгунавсегдапутинмедведевшойгунавсегдапутинмедведевшойгунавсегдапутинмедведевшойгунавсегдапутинмедведевшойгунавсегда", LocalDate.of(2005, 12, 22), 45)));
 
-        Assertions.assertEquals("Описание должно быть до 200 символов", exception.getMessage());
+        Assertions.assertEquals("class ru.yandex.practicum.filmorate.exception.EntityValidationException Описание должно быть до 200 символов", exception.getMessage());
     }
 
     @Test
     public void validationFilmThrowExceptionWhenDateBefore1985() {
-        FilmValidationException exception = Assertions.assertThrows(FilmValidationException.class, () ->
+        EntityValidationException exception = Assertions.assertThrows(EntityValidationException.class, () ->
                 FilmValidator.validationOfFilm(
                         new Film(1, "медвежуть", "эротодрамма", LocalDate.of(1892, 12, 22), 45)));
 
-        Assertions.assertEquals("Релиз должен быть не ранее 1895-12-28", exception.getMessage());
+        Assertions.assertEquals("class ru.yandex.practicum.filmorate.exception.EntityValidationException Релиз должен быть не ранее 1895-12-28", exception.getMessage());
     }
 
     @Test
     public void validationFilmThrowExceptionWhenDurationIsMinus() {
-        FilmValidationException exception = Assertions.assertThrows(FilmValidationException.class, () ->
+        EntityValidationException exception = Assertions.assertThrows(EntityValidationException.class, () ->
                 FilmValidator.validationOfFilm(
                         new Film(1, "медвежуть", "эротодрамма", LocalDate.of(1999, 12, 22), -2)));
 
-        Assertions.assertEquals("Длительность должна быть позитивной", exception.getMessage());
+        Assertions.assertEquals("class ru.yandex.practicum.filmorate.exception.EntityValidationException Длительность должна быть позитивной", exception.getMessage());
     }
 
     @Test
--- src/test/java/ru/yandex/practicum/filmorate/controller/UserValidatorTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/controller/UserValidatorTest.java
@@ -2,7 +2,7 @@ package ru.yandex.practicum.filmorate.controller;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import ru.yandex.practicum.filmorate.exception.UserValidationException;
+import ru.yandex.practicum.filmorate.exception.EntityValidationException;
 import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.validator.UserValidator;
 
@@ -14,29 +14,29 @@ class UserValidatorTest {
 
     @Test
     public void validationUserThrowExceptionWhenEmailWithOutDog() {
-        UserValidationException exception = Assertions.assertThrows(UserValidationException.class, () ->
+        EntityValidationException exception = Assertions.assertThrows(EntityValidationException.class, () ->
                 UserValidator.validationOfUsers(
                         new User(1, "11.ru", "erken", "besty", LocalDate.of(1992, 12, 01))));
 
-        Assertions.assertEquals("Нужно иметь почту или Собаку@", exception.getMessage());
+        Assertions.assertEquals("class ru.yandex.practicum.filmorate.exception.EntityValidationException Нужно иметь почту или Собаку@", exception.getMessage());
     }
 
     @Test
     public void validationUserThrowExceptionWhenLoginIsBlankOrSpace() {
-        UserValidationException exception = Assertions.assertThrows(UserValidationException.class, () ->
+        EntityValidationException exception = Assertions.assertThrows(EntityValidationException.class, () ->
                 UserValidator.validationOfUsers(
                         new User(1, "1@1.ru ", " ", "besty", LocalDate.of(1992, 12, 01))));
 
-        Assertions.assertEquals("Логин не должен быть пустым и без пробелов", exception.getMessage());
+        Assertions.assertEquals("class ru.yandex.practicum.filmorate.exception.EntityValidationException Логин не должен быть пустым и без пробелов", exception.getMessage());
     }
 
     @Test
     public void validationUserThrowExceptionWhenBirtDateInFuture() {
-        UserValidationException exception = Assertions.assertThrows(UserValidationException.class, () ->
+        EntityValidationException exception = Assertions.assertThrows(EntityValidationException.class, () ->
                 UserValidator.validationOfUsers(
                         new User(1, "1@1.ru", "erken", "besty", LocalDate.of(2026, 12, 1))));
 
-        Assertions.assertEquals("Нельзя родиться в будущем!", exception.getMessage());
+        Assertions.assertEquals("class ru.yandex.practicum.filmorate.exception.EntityValidationException Нельзя родиться в будущем!", exception.getMessage());
     }
 
     @Test
